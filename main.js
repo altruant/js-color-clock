03ff1c2b46c32787e49133784139fa94564c1363
altruant <noellehamberis@gmail.com>
2020-09-01 16:41:39 -0400

    progress clock implemented

--- main.js
+++ main.js
@@ -6,18 +6,20 @@ function currentTime() {
   let hours = date.getHours()
   let minutes = date.getMinutes();
   let seconds = date.getSeconds();
-  let percentSeconds = seconds/60;
+  const percentSeconds = seconds/60;
   hours = (hours > 12) ? hours - 12: hours; //converting return of 13-24 hrs to standard 12hr time
   hours = (hours == 0) ? 12 : hours; //converting midnight to 12 midnight
   hours = (hours < 10 ? "0": "") + hours; //appending a 0 in front of hours less than 10
   minutes = (minutes < 10 ? "0": "") + minutes; //appending a 0 in front of minutes less than 10
   seconds = (seconds < 10 ? "0": "") + seconds; //appending a 0 in front of seconds less than 10
   time = hours + ":" + minutes + ":" + seconds; //organizing time for display
-  document.querySelector("div.clock").innerHTML = time; //targeting the clock element to
+  document.querySelector("div.clock-display").innerHTML = time; //targeting the clock element to
   // console.log(time);
 
+  // progress bar width
   console.log(percentSeconds)
-
+  let bar = document.querySelector("div.clock-progress-bar");
+  bar.style.width = (percentSeconds * 14) + "rem"
   // random hexidecimal generator
 
 }
