c77850bdf6199a1a499386d9286a55b8aa379514
altruant <noellehamberis@gmail.com>
2020-09-01 15:39:39 -0400

    Live Clock and logging

--- main.js
+++ main.js
@@ -1,20 +1,25 @@
+
 function currentTime() {
   // creating a date constant to reference
   const date = new Date();
-  // hours, minutes, seconds initialized to a function of date
-  let h = date.getHours()
-  let m = date.getMinutes();
-  let s = date.getSeconds();
+  // hours, minutes, seconds initialized to get the current time
+  let hours = date.getHours()
+  let minutes = date.getMinutes();
+  let seconds = date.getSeconds();
+  let percentSeconds = seconds/60;
+  hours = (hours > 12) ? hours - 12: hours; //converting return of 13-24 hrs to standard 12hr time
+  hours = (hours == 0) ? 12 : hours; //converting midnight to 12 midnight
+  hours = (hours < 10 ? "0": "") + hours; //appending a 0 in front of hours less than 10
+  minutes = (minutes < 10 ? "0": "") + minutes; //appending a 0 in front of minutes less than 10
+  seconds = (seconds < 10 ? "0": "") + seconds; //appending a 0 in front of seconds less than 10
+  time = hours + ":" + minutes + ":" + seconds; //organizing time for display
+  document.querySelector("div.clock").innerHTML = time; //targeting the clock element to
+  // console.log(time);
 
-  h = (h == 0) ? h = 12 : h - 12;
-  h = (h < 0) ? "0" + h: h;
-  m = (m < 0) ? "0" + m: m;
-  s = (s < 0) ? "0" + s: s;
+  console.log(percentSeconds)
 
-  const time = h + ":" + m + ":" + s
+  // random hexidecimal generator
 
-  document.getElementByClassName("clock").innerText = time;
-
-  set(currentTime, 1000);
 }
 currentTime();
+setInterval(currentTime, 1000);
